2f9ea252a414d50ff8dc91f26e4623ba3c535f3e
jadilovic <adilovic79@yahoo.com>
2022-03-10 18:42:06 +0100

    for heroku

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('/api/:date?', function (req, res) {
 
 // listen for requests :)
 // port can be process.env.PORT
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT, function () {
 	console.log('Your app is listening on port ' + listener.address().port);
 });
